e9aad2f3d26766d16081d5c3788592e610cb33c8
Levene <Levene@TeacherMacLevene.local>
2021-06-17 22:35:50 -0400

    Adding the second assignment

---
+++ pre02/Date.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Date{
+    public static void main (String[] args){
+       
+
+        String day = "Tuesday";
+        String month = "June";
+        int date = 27;
+        int year = 2021;
+
+        System.out.println(day);
+        
+
+    }
+}
