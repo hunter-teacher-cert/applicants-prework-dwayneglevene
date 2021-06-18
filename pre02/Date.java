25cec3e4f5f808192eb09be11ccf6dbf0fdd40dd
Levene <Levene@TeacherMacLevene.local>
2021-06-18 00:06:30 -0400

    second project

--- pre02/Date.java
+++ pre02/Date.java
@@ -11,7 +11,13 @@ public class Date{
         int year = 2021;
 
         System.out.println(day);
-        
+        System.out.println(month);
+        System.out.println(date);
+        System.out.println(year);
+
+        System.out.println(day +", " + month +" " + date+ ", " +year);
+
+        System.out.println(day +", " + date +" " + month+ ", " +year);
 
     }
 }
